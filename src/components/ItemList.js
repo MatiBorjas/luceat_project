91e450ce89db67fd0b17ccb4f6adc0c83168ef6e
Matias Borjas <matias.borjas@gmail.com>
2022-05-02 07:09:31 -0300

    Logica en ItemListContainer, MAP en ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,26 +1,12 @@
-import { useState, useEffect } from "react"
-import { products } from "../data/products";
 import { Item } from "./Item";
 import '../styles/ItemList.css'
 
-export const ItemList = () => {
-
-  const [producto, setProducto] = useState([]);
-
-  useEffect(() => {
-    const getProducts = new Promise ((resolve, reject) => {
-      setTimeout (() => {resolve(products)}, 2000)
-    })
-
-    getProducts.then((result) => {
-      setProducto(result)
-      console.log('Promesa cumplida exitosamente', result)
-    })
-      .catch((err) => {console.log ('Hubo un error')})
-  
-  }, [])
+export const ItemList = ({ productos }) => {
 
   return (
-    <div className='cards'>{producto.map(prod => <Item key={prod.id} products={prod}/>)}</div>
+    <div className='cards'> {productos.map(prod => 
+      <Item key={prod.id} products={prod}/>
+      )}
+    </div>
   )
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
 import { ItemList } from "./ItemList"
+import { products } from "../data/products";
 import '../styles/ItemListContainer.css'
 
 function ItemListContainer() {
+
+    const [productos, setProductos] = useState([]);
+
+    useEffect(() => {
+    const getProducts = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(products)
+        }, 1000);
+    })
+
+    getProducts.then((result) => {
+      setProductos(result)
+      console.log('Promesa cumplida exitosamente', result)
+    })
+    .catch((err) => {
+      console.log('Hay habido un error')
+    });
+
+  }, [])
+
     return (
-    <div className='products-container'>
-        <ItemList />
-    </div>    
+      <div className='products-container'>
+        <ItemList productos={productos} />
+      </div>  
     )
+  
+    
 }
 
 export default ItemListContainer
