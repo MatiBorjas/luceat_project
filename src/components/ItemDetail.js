28707d487f61a36c94a86a158e46d25e273f8d2d
Matias Borjas <matias.borjas@gmail.com>
2022-06-04 12:35:15 -0300

    css a counter

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -22,25 +22,32 @@ export function ItemDetail({ detalles }) {
         <div className='detalle-titulo'>
           <h1>{detalles.nombre}</h1>
         </div>
-        <div className='elementos'>
-          <img src={detalles.imagen} className='imagen-producto' alt={detalles.nombre}/>
-          <div className='descripcion-precio'>
-            <div className='descripcion'>
-              <div className='texto'>{detalles.descripcion}</div>
-            </div>
-            <div className='valor'>
-              <div className='precio'>${detalles.precio} en efectivo</div>
+        <div className='contenedor-detalles'>
+          <div className='elementos'>
+            <img src={detalles.imagen} className='imagen-producto' alt={detalles.nombre}/>
+            <div className='descripcion-precio'>
+              <div className='descripcion'>
+                <div className='texto'>{detalles.descripcion}</div>
+              </div>
+              <div className='valor'>
+                <div className='precio'>${detalles.precio} en efectivo</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='contenedorCompra'>
-        {terminar ? (
-          <button><Link to={'/carrito'} className='terminarCompra'>Ir al carrito</Link></button>
-          ) :
-          <ItemCount initial={1}  stock={detalles.stock} onAdd={handleOnAdd} id={detalles.id}/>
-        }
+          <div className='contenedor-compra'>
+            {terminar ? (
+              <div className='contenedor-boton-irAlCarrito'>
+                <div className='btn-irAlCarrito'>
+                  <button><Link to={'/carrito'} className='terminarCompra'>Ir al carrito</Link></button>
+                </div>
+              </div>
+            ) :
+            <ItemCount initial={1}  stock={detalles.stock} onAdd={handleOnAdd} id={detalles.id}/>
+          }
+          </div>
         </div>
       </div>
+        
     </div>
   )
 }
