e8e6e16faeaa20b497dd24aad603ff7e526129a3
Matias Borjas <matias.borjas@gmail.com>
2022-06-10 10:57:02 -0300

    id de pedido

--- src/components/FormularioCompra.js
+++ src/components/FormularioCompra.js
@@ -3,43 +3,48 @@ import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { useCartContext } from '../context/CartContext';
 import '../styles/FormularioCompra.css';
 import Swal from "sweetalert2"
+import withReactContent from 'sweetalert2-react-content'
 
 function FormularioCompra() {
 
   const {cart, deleteCart} = useCartContext();
-
+  
   const [user, setUser] = useState({
     name: "",
     phone: "",
     email: "",
   });
 
+  const MySwal = withReactContent(Swal)
+
   const saveToFirestore = async (pedidos) => {
     const db = getFirestore();
-    const {id} = await addDoc(collection(db,"pedidos"), pedidos) 
+    const {id} = await addDoc(collection(db,"pedidos"), pedidos)
+    MySwal.fire({
+      title:'El no. de orden es:',
+      text: ` ${id}`,
+      icon:'warning',
+      confirmButtonText: "Anotado",
+      confirmButtonColor: "#e78848",
+    }).then(() => {
+      MySwal.fire({
+        title:'Gracias por tu compra',
+        text: 'Te vamos a estar contactando para coordinar el envio y la forma de pago',
+        icon:'success',
+        confirmButtonText: <a href='/'>Anotado</a>,
+        confirmButtonColor: "#e78848",
+      });}
+    );
   } 
-
 
   function onInputChange ({ id, value }){
     setUser({...user, [id]: value })
   }
 
-  const orderConfirmation = () => {
-    Swal.fire({
-      title:'Pedido enviado',
-      text:'Te contactamos para realizar el envio y resolver el pago',
-      icon:'success',
-      confirmButtonText: "Excelente!",
-      confirmButtonColor: "#e78848",
-      }
-    )
-  };
-
   function onSubmit(e){
     e.preventDefault();
     let productosCompra = cart.map(e => ({ nombre: e.nombre, precio: e.precio, cantidad:e.quantity }))
     saveToFirestore({user, productosCompra})
-    orderConfirmation();
     deleteCart();
   }
 
@@ -53,11 +58,11 @@ function FormularioCompra() {
             </div>
             <div className='datos-formulario'>
               <label id="name-label">Nombre y Apellido</label>
-                <input id="name" autocomplete='off' type="text" placeholder="Ingresa tu nombre completo" onChange={e => onInputChange(e.target) } required/>
+                <input id="name" autoComplete='off' type="text" placeholder="Ingresa tu nombre completo" onChange={e => onInputChange(e.target) } required/>
               <label id="phone-label">Telefono</label>
-                <input id="phone" autocomplete='off' type="text" placeholder="Ingresa tu n° de telefono" onChange={e => onInputChange(e.target) }required/>
+                <input id="phone" autoComplete='off' type="text" placeholder="Ingresa tu n° de telefono" onChange={e => onInputChange(e.target) }required/>
               <label id="email-label">E-mail</label>
-                <input id="email" autocomplete='off' type="email" placeholder="Ingresa tu email" onChange={e => onInputChange(e.target) }required/>
+                <input id="email" autoComplete='off' type="email" placeholder="Ingresa tu email" onChange={e => onInputChange(e.target) }required/>
               </div>
               <input type="submit" value="Enviar" className='btn-submit'/>
               </div>
